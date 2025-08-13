f6169ab82139ea33343eefae641231518cdb5cbf
S m Shovan <smshovan@scisoftbuild02.fnal.gov>
2025-08-13 10:59:57 -0500

    Add .gitignore and untrack build artifacts

--- build/tests/CMakeFiles/gen_test_rootfile.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for gen_test_rootfile.
-# This may be replaced when dependencies are built.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/smshovan/project2/RNTuple-I-O-Config/tests
-# Build directory: /home/smshovan/project2/RNTuple-I-O-Config/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_split_range_by_clusters "/home/smshovan/project2/RNTuple-I-O-Config/build/tests/test_split_range_by_clusters")
-set_tests_properties(test_split_range_by_clusters PROPERTIES  WORKING_DIRECTORY "/home/smshovan/project2/RNTuple-I-O-Config/build" _BACKTRACE_TRIPLES "/home/smshovan/project2/RNTuple-I-O-Config/tests/CMakeLists.txt;12;add_test;/home/smshovan/project2/RNTuple-I-O-Config/tests/CMakeLists.txt;0;")
